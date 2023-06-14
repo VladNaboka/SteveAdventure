3a3211e0185d44360add518885f86c69ae6fda2c
4Foshen <dk4ction@mail.ru>
2023-06-14 21:29:23 +0600

    Add guide

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    [SerializeField] private GameObject _guideScreen;
     public void StartGame()
     {
         SoundManager.instance.Play("Click");
         SceneManager.LoadScene("2048");
+    }
+    public void OpenGuide()
+    {
+        SoundManager.instance.Play("Click");
+        _guideScreen.SetActive(true);
+    }
+    public void CloseGuide()
+    {
+        SoundManager.instance.Play("Click");
+        _guideScreen.SetActive(false);
     }
 }
